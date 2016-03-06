570a4c600abea55bbefcc13ca3628868f86d5167
hunmin <hunmin@widerplanet.com>
2016-03-06 23:23:33 +0900

    a

--- torrentleeSpiderAv.py
+++ torrentleeSpiderAv.py
@@ -18,14 +18,14 @@ class BlogSpider(scrapy.Spider):
 #    start_urls = ['http://www.tosarang2.net/bbs/board.php?bo_table=torrent_kortv_drama&sca=&sop=and&sfl=wr_subject&stx=%ED%83%9C%EC%96%91%EC%9D%98+%ED%9B%84%EC%98%88']
 
     def parse(self, response):
-        Urllist = response.css('td.td_subject > a::attr("href")')
+        Urllist = response.css('li.li_subject > a::attr("href")')
         for url in Urllist.re(self.domain + "/bbs/board.php.*"):
 #            print url
             yield scrapy.Request(response.urljoin(url), self.parse_contents)
 
     def parse_contents(self, response):
         title = response.css('div#contents > div#bo_v > div#bo_v_title > h1::text').extract()
-        urlList = response.css('div#contents > div#bo_v > div.bo_v_file > a::attr("href")').re("magnet:.*&")
+        urlList = response.css('div#contents > div#bo_v > div.bo_v_link > a::attr("href")').re("magnet:.*&")
         createTime = response.css('div#contents > div#bo_v > div.bo_v_torrent > table > tr > td.value::text').re("[0-9]{4}-[0-9]{2}-[0-9]{2}.*")
         command = "transmission-remote 9091 -w " + self.path + " -a " + urlList[0]
         nowDateTime = datetime.now()
