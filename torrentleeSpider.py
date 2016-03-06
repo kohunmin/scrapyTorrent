e3946cb83a04e9e62543bd415316091a4bd57c8b
hunmin <hunmin@widerplanet.com>
2016-03-06 16:09:57 +0900

    add path

--- torrentleeSpider.py
+++ torrentleeSpider.py
@@ -10,7 +10,7 @@ class BlogSpider(scrapy.Spider):
         super(BlogSpider, self).__init__(*args, **kwargs)
         self.search = kwargs.get('search')
         self.seconds = int(kwargs.get('seconds'))
-        print self.seconds
+        self.path = kwargs.get('path')
         self.start_urls = ["http://www.tosarang2.net/bbs/board.php?bo_table=torrent_kortv_drama&sca=&sop=and&sfl=wr_subject&stx=" + quote(self.search)]
 
 #    start_urls = ['http://www.tosarang2.net/bbs/board.php?bo_table=torrent_kortv_drama&sca=&sop=and&sfl=wr_subject&stx=%ED%83%9C%EC%96%91%EC%9D%98+%ED%9B%84%EC%98%88']
@@ -25,7 +25,7 @@ class BlogSpider(scrapy.Spider):
         title = response.css('div#contents > div#bo_v > div#bo_v_title > h1::text').extract()
         urlList = response.css('div#contents > div#bo_v > div.bo_v_file > a::attr("href")').re("magnet:.*&")
         createTime = response.css('div#contents > div#bo_v > div.bo_v_torrent > table > tr > td.value::text').re("[0-9]{4}-[0-9]{2}-[0-9]{2}.*")
-        command = "transmission-remote 9091 -a " + urlList[0]
+        command = "transmission-remote 9091 -w " + self.path + " -a " + urlList[0]
         nowDateTime = datetime.now()
         createDateTime = datetime.strptime(createTime[0],'%Y-%m-%d %H:%M:%S')
         diffDateTime = nowDateTime - createDateTime
