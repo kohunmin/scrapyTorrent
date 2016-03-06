cc324ba1c74165f9981364e0a4331aca378b4b6a
hunmin <hunmin@widerplanet.com>
2016-03-06 18:42:03 +0900

    second 계산 문제가 있어 수정

--- torrentleeSpider.py
+++ torrentleeSpider.py
@@ -30,10 +30,10 @@ class BlogSpider(scrapy.Spider):
         command = "transmission-remote 9091 -w " + self.path + " -a " + urlList[0]
         nowDateTime = datetime.now()
         createDateTime = datetime.strptime(createTime[0],'%Y-%m-%d %H:%M:%S')
-        diffDateTime = nowDateTime - createDateTime
-        if diffDateTime.seconds < self.seconds :
+        diffSeconds = ( nowDateTime - createDateTime ).total_seconds()
+        if diffSeconds < self.seconds :
             print "[torrent]torrent download start"
-            print "title : " + title[0].encode("utf-8").strip() + "\nurl : " + response.request.url.encode("utf-8") + "\ncommand : " + command.encode("utf-8") + "\nseconds : " + str(diffDateTime.seconds)
+            print "title : " + title[0].encode("utf-8").strip() + "\nurl : " + response.request.url.encode("utf-8") + "\ncommand : " + command.encode("utf-8") + "\ncreatTime : " + createTime + "\ndiff seconds : " + str(diffSeconds)
             os.system(command.encode('utf-8'))
 
-        yield {"title" : title[0].encode("utf-8").strip(), "url" : response.request.url.encode("utf-8") , "command" : command.encode("utf-8") , "seconds" : diffDateTime.seconds }
+        yield {"title" : title[0].encode("utf-8").strip(), "url" : response.request.url.encode("utf-8") , "command" : command.encode("utf-8"), "createTime" : createTime , "seconds" : diffSeconds }
