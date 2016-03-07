f72cbb33c1f9cf6e1e6427dab93836ed3a1dfc2e
hunmin <hunmin@widerplanet.com>
2016-03-08 00:20:16 +0900

    a

--- torrentleeSpiderAv.py
+++ torrentleeSpiderAv.py
@@ -13,7 +13,9 @@ class BlogSpider(scrapy.Spider):
         self.seconds = int(kwargs.get('seconds'))
         self.path = kwargs.get('path')
         self.bo_table = kwargs.get('bo_table')
-        self.start_urls = [ self.domain + "/bbs/board.php?bo_table=" + self.bo_table + "&sca=&sop=and&sfl=wr_subject&stx=" + quote(self.search)]
+        url = self.domain + "/bbs/board.php?bo_table=" + self.bo_table + "&sca=&sop=and&sfl=wr_subject&stx=" + quote(self.search)
+        print "BUILDTORRENT " + url
+        self.start_urls = [ url ]
 
 #    start_urls = ['http://www.tosarang2.net/bbs/board.php?bo_table=torrent_kortv_drama&sca=&sop=and&sfl=wr_subject&stx=%ED%83%9C%EC%96%91%EC%9D%98+%ED%9B%84%EC%98%88']
 
@@ -32,12 +34,12 @@ class BlogSpider(scrapy.Spider):
         createDateTime = datetime.strptime(createTime,'%Y-%m-%d %H:%M:%S')
         diffSeconds = ( nowDateTime - createDateTime ).total_seconds()
         if diffSeconds < self.seconds :
-            print "[torrent]torrent download start"
-            print "title : " + title[0].encode("utf-8").strip()
-            print "\nurl : " + response.request.url.encode("utf-8")
-            print "\ncommand : " + command.encode("utf-8")
-            print "\ncreatTime : " + createTime
-            print "\ndiff seconds : " + str(diffSeconds)
+            print "INFOTORRENT [torrent]torrent download start"
+            print "INFOTORRENT title : " + title[0].encode("utf-8").strip()
+            print "\nINFOTORRENT url : " + response.request.url.encode("utf-8")
+            print "\nINFOTORRENT command : " + command.encode("utf-8")
+            print "\nINFOTORRENT creatTime : " + createTime
+            print "\nINFOTORRENT diff seconds : " + str(diffSeconds)
             os.system(command.encode('utf-8'))
 
         yield {"title" : title[0].encode("utf-8").strip(), "url" : response.request.url.encode("utf-8") , "command" : command.encode("utf-8"), "createTime" : createTime , "seconds" : diffSeconds }
