fd5dd687697b695f8b4a7d5670420dac997ffbd7
hunmin <hunmin@widerplanet.com>
2016-03-18 00:56:45 +0900

    aa

--- torrentleeSpider.py
+++ torrentleeSpider.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 class BlogSpider(scrapy.Spider):
     name = 'blogspider'
-    start_urls = ['http://www.tosarang2.net/bbs/board.php?bo_table=torrent_kortv_drama&sca=&sop=and&sfl=wr_subject&stx=%ED%83%9C%EC%96%91%EC%9D%98+%ED%9B%84%EC%98%88']
+    #start_urls = ['http://www.tosarang2.net/bbs/board.php?bo_table=torrent_kortv_drama&sca=&sop=and&sfl=wr_subject&stx=%ED%83%9C%EC%96%91%EC%9D%98+%ED%9B%84%EC%98%88']
     def __init__(self,*args,**kwargs):
         super(BlogSpider, self).__init__(*args, **kwargs)
         self.domain = kwargs.get('domain')
@@ -14,9 +14,9 @@ class BlogSpider(scrapy.Spider):
         self.seconds = int(kwargs.get('seconds'))
         self.path = kwargs.get('path')
         self.bo_table = kwargs.get('bo_table')
-        # url = self.domain + "/bbs/board.php?bo_table=" + self.bo_table + "&sca=&sop=and&sfl=wr_subject&stx=" + quote(self.search)
-        # print "BUILDTORRENT " + url
-        # self.start_urls = [ url ]
+        url = self.domain + "/bbs/board.php?bo_table=" + self.bo_table + "&sca=&sop=and&sfl=wr_subject&stx=" + quote(self.search)
+        print "BUILDTORRENT " + url
+        self.start_urls = [ url ]
 
 
     def parse(self, response):
